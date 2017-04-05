345c9e0594ebd8cd4308c92923df9496441f9f59
Francisco Roman <froman2@hawk.iit.edu>
2017-04-05 18:49:06 -0500

    added array containing objects for schedule output

--- js/site.js
+++ js/site.js
@@ -1,6 +1,11 @@
 var help = document.querySelector("#pass-reset");
 var fields = document.querySelectorAll("input");
-var reset = document.getElementById("pass-reset");
+var reset = document.getElementById("#pass-reset");
+var schedule = [{"course": "ITMD 362"}, 
+                {"buiding":"Stuart"}, 
+                {"days": "Mondays"},
+                {"time": "3:15-4:30"},
+                {"professor": "K. Stolley"}];
 
 for (var i=0; i < fields.length; i++) {
   fields[i].addEventListener("focus", function(event){
@@ -14,3 +19,16 @@ for (var i=0; i < fields.length; i++) {
     help.textContent = "";
   });
 }//end of for statement
+
+function logIn(){
+  var user = document.getElementById('email').value.trim();
+  var password = document.getElementById('password').value.trim();
+
+  if (user !== '' || password !== ''){
+    console.log("welcome to IIT " + user);
+  }else{
+    alert("fields cannot be empty");
+    help.textContent = "fields cannot be empty";
+  }
+}//end of logIn function
+document.getElementById('submit').addEventListener('click',logIn,false);
