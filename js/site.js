d8124a66cc2660d0c5382237fc796acdc8e17e3b
Francisco Roman <froman2@hawk.iit.edu>
2017-04-06 15:26:00 -0500

    added single course info as one object

--- js/site.js
+++ js/site.js
@@ -1,12 +1,18 @@
 var help = document.querySelector("#pass-reset");
 var fields = document.querySelectorAll("input");
 var reset = document.getElementById("#pass-reset");
-var schedule = [{"course": "ITMD 362"}, 
-                {"building":"Stuart"},
-                {"room":"301"}, 
-                {"days": "Monday"},
-                {"time": "3:15-4:30"},
-                {"professor": "K. Stolley"}];
+var schedule = [{"course": "ITMD-362", 
+                 "building": "Stuart",
+                 "room": "301",
+                 "days": "M",
+                 "time": "3:15-4:30",
+                 "professor": "K. Stolley"},
+                 {"course": "ITMD-430", 
+                 "building": "IIT South Tower",
+                 "room": "201",
+                 "days": "MW",
+                 "time": "3:15-4:30",
+                 "professor": "J. Hajek"}]; 
 
 for (var i=0; i < fields.length; i++) {
   fields[i].addEventListener("focus", function(event){
@@ -48,14 +54,14 @@ function makeSchedule() {
 function printSchedule(){
   var html = '<table class ="table">';
   html += '<tr><th>Course</th><th>Building</th><th>Room</th><th>Day</th><th>Time</th><th>Professor</th></tr>';
-  for (var i = 0; i < 1; i++){
+  for (var i = 0; i < schedule.length; i++){
     html += '<tr>';
     html += '<td>' + schedule[i].course + '</td>';
-    html += '<td>' + schedule[1].building + '</td>';
-    html += '<td>' + schedule[2].room + '</td>';
-    html += '<td>' + schedule[3].days + '</td>';
-    html += '<td>' + schedule[4].time + '</td>';
-    html += '<td>' + schedule[5].professor + '</td>';
+    html += '<td>' + schedule[i].building + '</td>';
+    html += '<td>' + schedule[i].room + '</td>';
+    html += '<td>' + schedule[i].days + '</td>';
+    html += '<td>' + schedule[i].time + '</td>';
+    html += '<td>' + schedule[i].professor + '</td>';
     html +='</tr>';
   }//end of for statement
   html += '</table>';
